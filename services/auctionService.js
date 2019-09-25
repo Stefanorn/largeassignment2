3c8a0ded8d2ab648eaf06a8f190003ee6564d1ac
stefanorn <stefanorn92@gmail.com>
2019-09-25 20:29:22 +0000

    added a winner finding stuff in get all auctions and get auctin by id

--- services/auctionService.js
+++ services/auctionService.js
@@ -13,14 +13,63 @@ const globalTryCatch = async cb => {
 const auctionService = () => {
     const getAllAuctions = async () => {
         return await globalTryCatch( async () =>{
-        return await AuctionDB.find({});
+            await AuctionDB.find({}).then(function (auctions) {
+               auctions.forEach( async function (auction) {
+
+                   // if i encounter a auction that is not expired, i will do nothing
+                   if( auction.endDate <= Date.now()){}
+
+                   // if i encounter a auction with winner then i will skip the extra step.
+                   else if(auction.auctionWinner != null){}
+                   // finaly i have encounterd a auction without a winner that should have a winner
+                       // so i will add the winner.
+                   else {
+                       const auctionWinner = await AuctionDBBID
+                           .find({"auctionId": auction._id})
+                           .findOne()
+                           .sort({price: '-1', endDate: '1'});
+
+
+                        await AuctionDB.findById(auction._id).updateOne(
+                           {},
+                           {$set: {"auctionWinner": auctionWinner.customerId}},
+                           {upsert: false, multi: true});
+                   }
+               });
+            });
+            return await AuctionDB.find({});
         });
     };
 
     const getAuctionById = async (id) => {
-        return await globalTryCatch( async() =>{
 
-            return AuctionDB.findById(id);
+        return await globalTryCatch( async() =>{
+            var auction = await AuctionDB.findById(id);
+
+            // if i encounter a auction that is not expired, there is no winner so i will return
+            // the auction as is.
+            if( auction.endDate <= Date.now()){
+                return await auction;
+            }
+            // if i encounter a auction with winner then i will skip the extra step and return.
+            else if(auction.auctionWinner != null){
+                return await auction;
+            }
+            // if i encounter a auction that dose not hae a winner i will add that row
+            else {
+                const auctionWinner = await AuctionDBBID
+                    .find({"auctionId": id})
+                    .findOne()
+                    .sort({price: '-1', endDate: '1'});
+
+                await AuctionDB.findById(id).updateOne(
+                    {},
+                    {$set: {"auctionWinner": auctionWinner}},
+                    {upsert: false, multi: true});
+
+                auction = await AuctionDB.findById(id);
+                return await auction;
+            }
         });
         
     };
